381227a348e73f9ddba3f7f6834f4de1754e4753
dmrl789 <ippan.org@gmail.com>
2025-06-07 14:55:52 +0200

    Fix: remove serde Deserialize from Wallet struct

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,46 +1,47 @@
-use ed25519_dalek::{SigningKey, VerifyingKey};
+use ed25519_dalek::SigningKey;
 use rand::rngs::OsRng;
-use bip39::Mnemonic;
 use bs58;
+use bip39::Mnemonic;
 use std::fs::File;
 use std::io::{Read, Write};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Wallet {
     pub signing_key: SigningKey,
-    //pub verifying_key: VerifyingKey,
     pub address: String,
 }
 
 impl Wallet {
     pub fn generate() -> Self {
-        let signing_key = SigningKey::generate(&mut OsRng);
-        let verifying_key = VerifyingKey::from(&signing_key);
-        let address = bs58::encode(verifying_key.as_bytes()).into_string();
+        let mut csprng = OsRng {};
+        let signing_key = SigningKey::generate(&mut csprng);
+        let public_key = signing_key.verifying_key();
+        let address = bs58::encode(public_key.as_bytes()).into_string();
 
         Wallet {
             signing_key,
-            verifying_key,
             address,
         }
     }
 
     pub fn save_to_file(&self, path: &str) {
         let sk_bytes = self.signing_key.to_bytes();
-        let mut file = File::create(path).expect("Unable to create wallet file");
-        file.write_all(&sk_bytes).expect("Unable to write wallet file");
+        let address = &self.address;
+        let mut file = File::create(path).expect("Unable to create file");
+        file.write_all(&sk_bytes).expect("Unable to write signing key");
+        file.write_all(address.as_bytes()).expect("Unable to write address");
     }
 
     pub fn load_from_file(path: &str) -> Self {
-        let mut file = File::open(path).expect("Unable to open wallet file");
+        let mut file = File::open(path).expect("Unable to open file");
         let mut sk_bytes = [0u8; 32];
-        file.read_exact(&mut sk_bytes).expect("Unable to read wallet file");
+        file.read_exact(&mut sk_bytes).expect("Unable to read signing key");
+        let mut address_bytes = Vec::new();
+        file.read_to_end(&mut address_bytes).expect("Unable to read address");
         let signing_key = SigningKey::from_bytes(&sk_bytes);
-        let verifying_key = VerifyingKey::from(&signing_key);
-        let address = bs58::encode(verifying_key.as_bytes()).into_string();
+        let address = String::from_utf8(address_bytes).unwrap_or_default();
         Wallet {
             signing_key,
-            verifying_key,
             address,
         }
     }
